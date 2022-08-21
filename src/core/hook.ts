588e3301ea1c321181f0f9277008ac5dbf7a138b
GeneRich <80384658+webgen1@users.noreply.github.com>
2022-08-21 20:51:31 +0300

    Delete hook.ts
    Delete hook

--- src/core/hook.ts
+++
@@ -1,8 +0,0 @@
-// Imports
-import { useDispatch, useSelector, TypedUseSelectorHook} from 'react-redux';
-import type { RootState, AppDispatch } from './store';
-
-
-// Custom hooks with typescript
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
