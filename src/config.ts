66452240fb2da651ca71d8f89712b6fac9bf1e34
Nikolay Korzhikov <45668685+Lil-Korzhik@users.noreply.github.com>
2022-12-05 01:54:46 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 const DEVELOPMENT_NEXT_API_URL: string = 'http://localhost:3000/api';
-const PRODUCTION_NEXT_API_URL: string = 'https://tween-client-gennadiy.vercel.app/api';
+const PRODUCTION_NEXT_API_URL: string = 'https://tween-blog.online/api';
 
-const EXPRESS_URL: string = 'https://express-api-production-2f1f.up.railway.app';
+const EXPRESS_URL: string = 'https://api.tween-blog.online';
 
 const config =  {
-    EXPRESS_API_URL: `${EXPRESS_URL}/api`,
+    EXPRESS_API_URL: `${EXPRESS_URL}`,
     NEXT_API_URL: process.env.NODE_ENV === 'production' ? PRODUCTION_NEXT_API_URL : DEVELOPMENT_NEXT_API_URL,
     PREVIEW_URL: `${EXPRESS_URL}/preview.png`,
     AVATARS_URL: `${EXPRESS_URL}/avatars/`,
